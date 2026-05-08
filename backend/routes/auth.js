dcb2208a9753c38c6a56dc570733515300b47213
sai rohit <sairohit.tatavarthi@gmail.com>
2026-05-08 19:02:16 +0530

    learnt about the findone

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -8,19 +8,31 @@ router.post('/createuser',[
     body('name','Enter a valid name').isLength({min : 3}),
     body('email','Enter a valid email').isEmail(),
     body('password','Enter password upto 5 characters').isLength({min : 5}),
-],(req,res) => {
+], async (req,res) => {
     const errors = validationResult(req);
     if(!errors.isEmpty()){
         return res.status(400).json({errors : errors.array()})
     }
-    User.create({
+
+    try{
+    let user = await User.findOne({email: req.body.email});
+    console.log(user);
+    if(user){
+        return res.status(400).json({error : 'Sorry! a user with this email already exists'})
+    }
+    user = await User.create({
         name: req.body.name,
         email: req.body.email,
         password: req.body.password,
-    }).then(user => res.json(user))
-    .catch(err => {
-        res.json({error : 'Please enter a unique value for email',message : err.message})
-    });
-
+    })
+    // .then(user => res.json(user))
+    // .catch(err => {
+    //     res.json({error : 'Please enter a unique value for email',message : err.message})
+    // });
+    res.json(user);
+    } catch(error){
+        console.error(error.message);
+        res.status(500).send("Some error occured");
+    }
 })
 module.exports = router
